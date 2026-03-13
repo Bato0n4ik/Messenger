72ff3c9e12fce3a89487e5506969a8a0bee92d30
Bato0n4ik <bato0n4ik.47@gmail.com>
2026-03-13 05:28:05 +0300

    add MongoAutoStartConfig class for auto starting and autoclosing mongod.exe, add connection properties for mongodb in application.yml

---
+++ src/main/java/com/andrew/messenger/configurations/MongoAutoStartConfig.java
@@ -0,0 +1,75 @@
+package com.andrew.messenger.configurations;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.Socket;
+
+@Configuration
+public class MongoAutoStartConfig {
+
+    @Value("${mongodb.exe.path}")
+    private String mongoExePath;
+
+    @Value("${mongodb.data.path}")
+    private String mongoDataPath;
+
+    private Process mongoProcess;
+
+    @PostConstruct
+    public void startMongo() {
+
+        if (isPortInUse("localhost", 27017)) {
+            System.out.println(">>> MongoDB уже запущена на порту 27017. Используем существующий экземпляр.");
+            return;
+        }
+
+        try {
+            File dbDir = new File(mongoDataPath);
+            if (!dbDir.exists()) {
+                boolean created = dbDir.mkdirs();
+                if (created) {
+                    System.out.println("--- Папка для БД создана: " + mongoDataPath);
+                }
+            }
+
+
+            ProcessBuilder pb = new ProcessBuilder(
+                    mongoExePath,
+                    "--dbpath", mongoDataPath,
+                    "--port", "27017" // Стандартный порт
+            );
+
+
+            pb.inheritIO();
+
+            // 3. Запускаем
+            this.mongoProcess = pb.start();
+            System.out.println("--- MongoDB запущена автоматически ---");
+
+        } catch (IOException e) {
+            System.err.println("!!! Не удалось запустить MongoDB: " + e.getMessage());
+            System.err.println("Проверьте путь к mongod.exe и права доступа к папке.");
+        }
+    }
+
+    private boolean isPortInUse(String host, int port) {
+        try (Socket ignored = new Socket(host, port)) {
+            return true;
+        } catch (IOException e) {
+            return false;
+        }
+    }
+
+    @PreDestroy
+    public void stopMongo() {
+        if (mongoProcess != null && mongoProcess.isAlive()) {
+            mongoProcess.destroy();
+            System.out.println("--- MongoDB остановлена вместе с приложением ---");
+        }
+    }
+}
--- src/main/java/com/andrew/messenger/http/controllers/LoginController.java
+++ src/main/java/com/andrew/messenger/http/controllers/LoginController.java
@@ -1,54 +1,14 @@
 package com.andrew.messenger.http.controllers;
 
-import com.andrew.messenger.database.entity.User;
-import com.andrew.messenger.dto.LoginDto;
-import com.andrew.messenger.service.UserService;
-import com.andrew.messenger.validators.LoginValidator;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/login")
 @RequiredArgsConstructor
 public class LoginController {
-
-    //public final UserService userService;
-    //public final LoginValidator loginValidator;
-
-    /*@GetMapping
-    public String login(LoginDto loginDto, Model model){
-        model.addAttribute("loginDto", loginDto);
-        return "user/login";
-    }/*
-
-    /*@PostMapping
-    public String loginPost(@ModelAttribute @Validated LoginDto loginDto,
-                            BindingResult bindingResult,
-                            RedirectAttributes redirectAttributes){
-
-        if(bindingResult.hasErrors()){
-            redirectAttributes.addFlashAttribute("errors", bindingResult);
-            redirectAttributes.addFlashAttribute("loginDto", loginDto);
-            return "redirect:/login";
-        }
-        else if(!loginValidator.validate(loginDto)){
-            return "redirect:/login";
-        }
-
-        return userService.findByUsername(loginDto.getUsername())
-                .map(User::getId)
-                .map(id -> "redirect:/users/"+ id)
-                .orElseThrow(() -> new UsernameNotFoundException(loginDto.getUsername()));
-    }*/
 
     @GetMapping
     public String login() {
